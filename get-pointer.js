a00f4ac6ac0a41301543c297312a5994de06b7a6
Erik <enoex1@gmail.com>
2015-02-22 23:27:00 -0800

    initial tests

---
+++ get-pointer.js
@@ -0,0 +1,58 @@
+(function getPointer(){
+    var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+    if ( havePointerLock ) {
+        var instructions = document.getElementById( 'instructions' );
+        var blocker = document.getElementById( 'blocker' );
+        var element = document.body;
+
+        var pointerlockchange = function ( event ) {
+            if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+                controls.enabled = true;
+                blocker.style.display = 'none';
+
+            } else {
+                controls.enabled = false;
+                instructions.style.display = '';
+            }
+        }
+
+        var pointerlockerror = function ( event ) {
+            instructions.style.display = '';
+            alert('Error getting pointer');
+        };
+
+        // Hook pointer lock state change events
+        document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+        document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+        document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+        document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+        document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+        document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+        instructions.addEventListener( 'click', function ( event ) {
+            instructions.style.display = 'none';
+
+            // Ask the browser to lock the pointer
+            element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+            if (/Firefox/i.test( navigator.userAgent ) ) {
+                var fullscreenchange = function ( event ) {
+                    if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+                        document.removeEventListener( 'fullscreenchange', fullscreenchange );
+                        document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+                        element.requestPointerLock();
+                    }
+                }
+                document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+                document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+                element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+                element.requestFullscreen();
+            } else {
+                element.requestPointerLock();
+            }
+        }, false );
+
+    } else {
+        alert('Your browser does not support this. Get chrome');
+    }
+})();
